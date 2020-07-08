1eb5dc4090fa8f3fc381b836d5d4ba8253749e13
Enthusiastic <60442667+harshita897@users.noreply.github.com>
2020-07-08 21:20:02 +0530

    Add files via upload

---
+++ sharp.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+imagedata_original = cv2.imread('three.png')
+
+cv2.imshow('original image' , imagedata_original)
+cv2.waitKey(0)
+
+sharpening_filter = np.array([[-1,-1,-1],
+                                [-1,9,-1],
+                                [-1,-1,-1]])
+
+sharpened_image = cv2.filter2D(imagedata_original, -1, sharpening_filter)
+
+cv2.imshow('sharpened image', sharpened_image)
+cv2.imwrite('neweye.png',sharpened_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
